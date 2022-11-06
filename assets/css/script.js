4c8223d5daebe13a764c243008144e91d445fca2
JaredIng87 <jaredingersoll14@gmail.com>
2022-11-06 11:13:35 -0500

    added javascript

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,3 +1,26 @@
+var today = moment().format('[Today is ]MMM Do YYYY');
+var now = moment().hour();
+var element = document.querySelectorAll(".time-block");
+
+
+$("#currentDay").text(today);
+
+console.log(text);
+/**function checkTime() {
+        if (hour > now) {
+
+            element.classList.add('future');
+        }
+        else if (hour == now) {
+            element.classList.add('present');
+        }
+        else {
+            element.classList.add('past');
+        }
+};*/
+
+
+//checkTime();
 
 
 
