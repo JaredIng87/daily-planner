a47608f290075cdcb6eeaa0ce1b1e455890a6518
JaredIng87 <jaredingersoll14@gmail.com>
2022-12-03 17:49:44 -0500

    button functionality

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,19 +1,20 @@
 var today = moment().format('[Today is ]MMM Do YYYY');
 var now = moment().hour();
 var timeBlock = document.querySelectorAll(".time-block");
-
+var saveButton = document.querySelectorAll(".btn");
+var toBeDoneSpan = document.querySelectorAll(".text");
 
 
 $("#currentDay").text(today);
 
 
 for (var i = 0; i < timeBlock.length; i++) {
-    var state = timeBlock[i].getAttribute("data-number");
-        console.log(state);
-    if (state < now) {
+    var block = timeBlock[i].getAttribute("data-number");
+        console.log(block);
+    if (block < now) {
         timeBlock[i].classList.add("past");
     }
-    else if (state == now) {
+    else if (block == now) {
         timeBlock[i].classList.add("present");
     }
     else {
@@ -21,4 +22,8 @@ for (var i = 0; i < timeBlock.length; i++) {
     }
 }
 
-
+for (var i = 0; i < saveButton.length; i++) {
+saveButton[i].addEventListener("click", function() {
+    console.log("hello");
+  });
+};
